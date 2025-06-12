587dd3b21814e2ef6c2b78c3c33904bf554aded3
WebSurfinMurf <websurfinmurf@gmail.com>
2025-06-11 21:18:58 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,12 @@ set -e
 echo "Pulling latest from GitHub..."
 git pull origin main
 
+echo "Loading environment variables..."
+set -a
+source /home/websurfinmurf/projects/secrets/postgres.env
+set +a
+
 echo "Starting PostgreSQL container..."
-docker-compose  --env-file /home/websurfinmurf/projects/secrets/postgres.env up -d
+docker compose up -d
 
 echo "Deployment complete."
