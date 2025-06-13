baedfefdb07554060c91e336a36dcb2083481518
WebSurfinMurf <websurfinmurf@gmail.com>
2025-06-12 20:15:45 -0400

    Update deployui.sh

--- deployui.sh
+++ deployui.sh
@@ -7,7 +7,7 @@ source /home/websurfinmurf/projects/secrets/postgres.env
 set +a
 
 docker run -d \
-  --name pgadmin \
+  --name "$PGADMIN_CONTAINER_NAME" \
   -e PGADMIN_DEFAULT_EMAIL="$PGADMIN_EMAIL" \
   -e PGADMIN_DEFAULT_PASSWORD="$PGADMIN_PASSWORD" \
   -p 8080:80 \
