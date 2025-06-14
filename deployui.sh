26b06f1cf54cb1e4010f721a7cf5e7bd1278664c
WebSurfinMurf <websurfinmurf@gmail.com>
2025-06-13 21:22:41 -0400

    Update deployui.sh

--- deployui.sh
+++ deployui.sh
@@ -10,5 +10,5 @@ docker run -d \
   --name "$PGADMIN_CONTAINER_NAME" \
   -e PGADMIN_DEFAULT_EMAIL="$PGADMIN_EMAIL" \
   -e PGADMIN_DEFAULT_PASSWORD="$PGADMIN_PASSWORD" \
-  -p 8080:8901 \
+  -p 80:8901 \
   dpage/pgadmin4
