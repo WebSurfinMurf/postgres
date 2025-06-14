b4352b8182a7d8ac321de0be642ab6b49a81b728
WebSurfinMurf <websurfinmurf@gmail.com>
2025-06-13 22:11:33 -0400

    Update deployui.sh

--- deployui.sh
+++ deployui.sh
@@ -10,6 +10,7 @@ docker run -d \
   --name "$PGADMIN_CONTAINER_NAME" \
   -e PGADMIN_DEFAULT_EMAIL="$PGADMIN_EMAIL" \
   -e PGADMIN_DEFAULT_PASSWORD="$PGADMIN_PASSWORD" \
+  -e PGPASSFILE=/home/pgadmin/.pgpass \
   -v /home/websurfinmurf/projects/secrets/postgresservers.json:/pgadmin4/servers.json \
   -v /home/websurfinmurf/projects/secrets/.pgpass:/home/pgadmin/.pgpass \
   -p 8901:80 \
